5130f9b834ebc87b8a0eeb353dd290b1e70a7070
EnzoCam7 <camerac09@gmail.com>
2023-02-07 19:01:33 -0300

    New Services

--- src/services/diary-serv.ts
+++ src/services/diary-serv.ts
@@ -1,5 +1,35 @@
 import diaryData from './diaries.json'
+import { DiaryEntry, newDiaryEntry, NoCommentsEntry } from '../types'
 
-export const getEntries = () => diaryData
+//Aclaramos que el JSON lo debe tratar como el tipo que le pasamos.
+const diaries: DiaryEntry[] = diaryData as DiaryEntry[] 
 
-export const addData = () => null
+export const getEntries = () : DiaryEntry[] => diaries
+
+export const findById = (id:number) : NoCommentsEntry | undefined =>{
+    const entry = diaries.find(diary => diary.id === id)
+    return entry
+}
+
+export const getEntriesWithoutComments = () : NoCommentsEntry[] => {
+    return diaries.map(({id, date, weather, visibility}) =>{
+        return{
+            id,
+            date,
+            weather,
+            visibility
+        }
+    })
+}
+
+
+//El nuevo diary retorna un objeto de tipo NDE q termina siendo DiaryEntry debido a que se le suma la ID.
+export const addDiary = (newDiaryEntry : newDiaryEntry) : DiaryEntry => {
+    const newDiary = {
+        id: diaries.length + 1,
+        ...newDiaryEntry
+    }
+
+    diaries.push(newDiary)
+    return newDiary
+}
