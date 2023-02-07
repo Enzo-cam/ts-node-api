fcf1184ca1500067476356a84868fe5a78f408c5
EnzoCam7 <camerac09@gmail.com>
2023-02-07 19:01:14 -0300

    New Types

--- src/types.d.ts
+++ src/types.d.ts
@@ -2,11 +2,17 @@ export type Weather = 'sunny' | 'rainy' | 'cloudy' | 'windy' | 'stormy'
 export type Visibility = 'great' | 'good' | 'ok' | 'poor'
 
 //La interfaz esta pensada para ser extendida, en sentido de que se le pueden agregar
-//mas propiedas sumadas a las que ya tiene
+//mas propiedas sumadas a las que ya tiene.
 export interface DiaryEntry {
     id: number,
     date: string,
     weather: Weather,
-    visibily: Visibility,
+    visibility: Visibility,
     comment: string
 }
+
+//Cualquiera de las dos es valida
+//export type NoCommentsEntry = Pick<DiaryEntry, 'id' | 'date' | 'weather' | 'visibility'>
+export type NoCommentsEntry = Omit<DiaryEntry, 'comment'>
+
+export type newDiaryEntry = Ommit<DiaryEntry, 'id'>
